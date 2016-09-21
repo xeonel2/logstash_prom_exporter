cf8cb3c93ba9ee348727f8de959966c64905ef83
Venkatakrishnan <venkatakrishnan@exotel.in>
2016-09-21 13:55:07 +0530

    Added Metrics to be configured by yaml file

--- logstashexporter.go
+++ logstashexporter.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -7,37 +6,29 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gopkg.in/yaml.v2"
+	"github.com/deckarep/golang-set"
 	"net/http"
-	"strings"
 	"strconv"
+	"strings"
 	"regexp"
 	"io/ioutil"
 	"log"
 )
 
-//type ExtraStuff map[string] interface{}
-type jsonData struct {
-	LogMessages []Messages `json:"log_message"`
-	ProcessName string `json:"process_name"`
-	HostName    string `json:"host_name"`
-	ThreadID   string `json:"thread_id"`
-	RequestFull   string `json:"request"`
-	ProcessID   string `json:"process_id"`
-	Level       string `json:"level"`
-	LogTime     string `json:"time"`
-	//FileName    string
-	//LineNum     int
-}
-
-
-type Messages struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
-}
-
 type conf struct {
 	Regex string `yaml:"regex"`
+	Metrics []Metric `yaml:"metrics"`
 }
+
+type Metric struct{
+	KeyName string `yaml:"keyname"`
+	MetricType string `yaml:"metrictype"`
+	MetricName string `yaml:"metricname"`
+	Help string `yaml:"help"`
+	Labels []string `yaml:"labels"`
+}
+
+type MapStringInterface map[string]interface{}
 
 func (c *conf) getConf() *conf {
 	yamlFile, err := ioutil.ReadFile("logex.yml")
@@ -51,154 +42,124 @@ func (c *conf) getConf() *conf {
 	return c
 }
 
+//Function to handle a log message received on /post
 func parseGhPost(rw http.ResponseWriter, request *http.Request) {
-    	decoder := json.NewDecoder(request.Body)
-	var t jsonData
-	err := decoder.Decode(&t)
+	//A temporary map to store the values for each label
+	var labelvaluemap = make(map[string] string)
+	var mapstringinterface MapStringInterface
+	decoder := json.NewDecoder(request.Body)
+	err := decoder.Decode(&mapstringinterface)
 	if err != nil {
-		fmt.Println("Not recognized as monitoring message!",err.Error())
-		return
+		fmt.Println("Error JSON Decoding!", err.Error())
 	}
-	  /*newt, err := json.Marshal(&t)
-            if err != nil {
-                    fmt.Println("Error Converting to Json!")
-                }
-            fmt.Println(string(newt))*/
-	    //Creating Endpoint labels
-	    	var RequestEndpoint string
-		Regexp := regexp.MustCompile(con.Regex)
-		if(string(t.RequestFull)==""){
-			//Request field empty
-		}else
-		{
-			RequestEndpoint = Regexp.FindString(string(t.RequestFull))
+
+	//Iterate through all labels and check if it is present in the log entry
+	for _, label := range AllLabels.ToSlice() {
+		if value, ok := mapstringinterface[label.(string)]; ok {
+			fmt.Printf("\nLabel present : %+v", label)
+			if _, ok := value.([]interface{}); ok {
+				//Array of interfaces
+			}else{
+				//Some Label value ... Need to add necessary stuff
+
+					//fmt.Printf("Not a []interafce{} : %+v", value)
+					//fmt.Print("::", reflect.TypeOf(value))
+
+					labelvaluemap[label.(string)] = value.(string)
+					fmt.Println(labelvaluemap[label.(string)])
+
+
+
+			}
+		}else if value, ok := mapstringinterface["request"]; ok {
+			var RequestEndpoint string
+			Regexp := regexp.MustCompile(con.Regex)
+			if(string(value.(string))==""){
+				//Request field empty
+			}else
+			{
+				RequestEndpoint = Regexp.FindString(string(value.(string)))
+			}
+			RequestEndpoint = strings.Replace(RequestEndpoint, " ", "_", -1)
+			labelvaluemap["request_endpoint"] = RequestEndpoint
+
+		}else{
+				//Label not present
 		}
-		RequestEndpoint = strings.Replace(RequestEndpoint, " ", "_", -1)
 
-            for _, message := range t.LogMessages {
-                switch string(message.Key) {
-                case "latency":
-                    setvalue, err := strconv.ParseFloat(message.Value, 64)
-                    if err != nil {
-                        panic(err)
-                    }
-                    Latency.WithLabelValues(string(t.ProcessName), string(t.HostName), string(t.ThreadID), RequestEndpoint).Set(setvalue)
-		case "request_time":
-                    setvalue, err := strconv.ParseFloat(message.Value, 64)
-                    if err != nil {
-                        panic(err)
-                    }
-		    RequestTime.WithLabelValues(string(t.ProcessName), string(t.HostName), string(t.ThreadID), RequestEndpoint).Set(setvalue)
-		case "upstream_response_time":
-                    setvalue, err := strconv.ParseFloat(message.Value, 64)
-                    if err != nil {
-                        panic(err)
-                    }
-	            UpstreamResponseTime.WithLabelValues(string(t.ProcessName), string(t.HostName), string(t.ThreadID), RequestEndpoint).Set(setvalue)
-		case "status":
-                    setvalue, err := strconv.ParseFloat(message.Value, 64)
-                    if err != nil {
-                        panic(err)
-                    }
-		    if (setvalue >= 500 && setvalue < 600){
-			    Http5XXcode.WithLabelValues(string(t.ProcessName), string(t.HostName), string(t.ThreadID), RequestEndpoint).Inc()} else if
-		    (setvalue >= 400 && setvalue < 500){
-			    Http4XXcode.WithLabelValues(string(t.ProcessName), string(t.HostName), string(t.ThreadID), RequestEndpoint).Inc()} else if
-	 	    (setvalue >= 200 && setvalue < 300){
-			    Http2XXcode.WithLabelValues(string(t.ProcessName), string(t.HostName), string(t.ThreadID), RequestEndpoint).Inc()}
-                case "http_5xx":
-                    Http5XXcode.WithLabelValues(string(t.ProcessName), string(t.HostName), string(t.ThreadID), RequestEndpoint).Inc()
-                case "http_4xx":
-                    Http4XXcode.WithLabelValues(string(t.ProcessName), string(t.HostName), string(t.ThreadID), RequestEndpoint).Inc()
-                case "http_2xx":
-                    Http2XXcode.WithLabelValues(string(t.ProcessName), string(t.HostName), string(t.ThreadID), RequestEndpoint).Inc()
-                case "request_count":
-                    RequestCount.WithLabelValues(string(t.ProcessName), string(t.HostName), string(t.ThreadID), RequestEndpoint).Inc()
-                }
-            }
+	}
 
-}
-
-func indexPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("")
-}
-
-var (
-	Latency = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "logstash_latency",
-		Help: "Latency of the service.",
-	},
-		[]string{"processname", "hostname", "processid"},
-	)
-	RequestTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "logstash_request_time",
-		Help: "Nginx request time.",
-	},
-		[]string{"processname", "hostname", "processid", "request_endpoint"},
-	)
-	UpstreamResponseTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "logstash_upstream_response_time",
-		Help: "Nginx Upstream response time.",
-	},
-		[]string{"processname", "hostname", "processid", "request_endpoint"},
-	)
-	BodyBytesSent = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "logstash_body_bytes_sent",
-		Help: "Nginx Size of body.",
-	},
-		[]string{"processname", "hostname", "processid", "request_endpoint"},
-	)
-	RequestCount = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "logstash_request_count",
-			Help: "Number of Requests.",
-		},
-		[]string{"processname", "hostname", "processid", "request_endpoint"},
-	)
-	Http5XXcode = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "logstash_http_5xx_count",
-			Help: "Number of 5xx status codes.",
-		},
-		[]string{"processname", "hostname", "processid", "request_endpoint"},
-	)
-	Http2XXcode = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "logstash_http_2xx_count",
-			Help: "Number of 2xx status codes.",
-		},
-		[]string{"processname", "hostname", "processid", "request_endpoint"},
-	)
-	Http4XXcode = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "logstash_http_4xx_count",
-			Help: "Number of 4xx status codes.",
-		},
-		[]string{"processname", "hostname", "processid", "request_endpoint"},
-	)
-)
-
-func init() {
-	// Metrics have to be registered to be exposed:
-	prometheus.MustRegister(Latency)
-	prometheus.MustRegister(RequestTime)
-	prometheus.MustRegister(UpstreamResponseTime)
-	prometheus.MustRegister(BodyBytesSent)
-	prometheus.MustRegister(RequestCount)
-	prometheus.MustRegister(Http5XXcode)
-	prometheus.MustRegister(Http2XXcode)
-	prometheus.MustRegister(Http4XXcode)
-	fmt.Println("Prometheues metrics registered...")
+	if value, ok := mapstringinterface["log_message"]; ok {
+		fmt.Println("\nLabel present : log_message")
+		if element, ok := value.([]interface{}); ok {
+			//Array of interfaces
+			for _, message := range element {
+				if blahMap, ok := message.(map[string]interface{}); ok {
+					//fmt.Printf("\n%+v has value %+v\n", blahMap["key"], blahMap["value"])
+					var templabelarray []string
+					for _, lname:= range LabelsMap[blahMap["key"].(string)]{
+						templabelarray = append(templabelarray,labelvaluemap[lname])
+					}
+					if _, ok := CounterMap[blahMap["key"].(string)]; ok {
+						CounterMap[blahMap["key"].(string)].WithLabelValues(templabelarray...).Inc()
+				}else if _, ok := GaugeMap[blahMap["key"].(string)]; ok {
+					setvalue, err := strconv.ParseFloat(blahMap["value"].(string), 64)
+					if err != nil {
+						panic(err)
+					}
+					GaugeMap[blahMap["key"].(string)].WithLabelValues(templabelarray...).Set(setvalue)
+				}
+				}else {
+					fmt.Printf("Not a map[string]interface{}\n%+v\n", message)
+				}
+			}
+		}
+	}
 }
 
 var con *conf
+var CounterMap = make(map[string] *prometheus.CounterVec)
+var GaugeMap = make(map[string] *prometheus.GaugeVec)
+var LabelsMap = make(map[string] []string)
+var KeyNames []string
+var AllLabels = mapset.NewSet()
 
 func main() {
 	con = new(conf)
 	con.getConf()
+
+	fmt.Println("Registering Prometheus Metrics...")
+	//fmt.Printf("%+v", con)
+
+	for _,element := range con.Metrics {
+		if (element.MetricType=="counter"){
+			CounterMap[element.KeyName] = prometheus.NewCounterVec(prometheus.CounterOpts{Name : element.MetricName,Help: element.Help}, element.Labels)
+			prometheus.MustRegister(CounterMap[element.KeyName])
+			LabelsMap[element.KeyName]= element.Labels
+			KeyNames=append(KeyNames,element.KeyName)
+			old := element.Labels
+			new := make([]interface{}, len(old))
+			for i, v := range old {
+				new[i] = v
+			}
+			AllLabels = AllLabels.Union(mapset.NewSetFromSlice(new))
+		}else if(element.MetricType=="gauge"){
+			GaugeMap[element.KeyName] = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name : element.MetricName, Help: element.Help}, element.Labels)
+			prometheus.MustRegister(GaugeMap[element.KeyName])
+			LabelsMap[element.KeyName]= element.Labels
+			KeyNames=append(KeyNames,element.KeyName)
+			old := element.Labels
+			new := make([]interface{}, len(old))
+			for i, v := range old {
+				new[i] = v
+			}
+			AllLabels = AllLabels.Union(mapset.NewSetFromSlice(new))
+		}
+	}
+
+	fmt.Println("Metrics successfully registered!")
 	fmt.Println("Starting Http server...")
 	http.HandleFunc("/post", parseGhPost)
-	http.HandleFunc("/", indexPage)
 	http.Handle("/metrics", promhttp.Handler())
 	if err := http.ListenAndServe("0.0.0.0:8000", nil); err != nil {
 		fmt.Println("Failed to make connection" + err.Error())
